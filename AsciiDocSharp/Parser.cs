2aceb5c435e90d681147cd5e4749bddf8d695ac5
Suprnova <nova@suprnova.dev>
2024-10-01 17:13:56 -0400

    Document and modify access of Parser
    Breaking changes:
    Most fields and methods are now private or protected. The main entry point, Parse, is still public.
    The names of certain Regex patterns have been renamed to be more verbose.

--- AsciiDocSharp/Parser.cs
+++ AsciiDocSharp/Parser.cs
@@ -5,13 +5,31 @@ namespace AsciiDocSharp
 {
     public partial class Parser
     {
-        public class QuoteRegex(Regex pattern, SpanVariant variant)
+		/// <summary>
+		/// Represents a relationship between a <see cref="Regex"/> pattern and its corresponding <see cref="SpanVariant"/>.
+		/// </summary>
+		/// <param name="pattern"><inheritdoc cref="Pattern" path="/summary"/></param>
+		/// <param name="variant"><inheritdoc cref="Variant" path="/summary"/></param>
+		protected class QuoteRegex(Regex pattern, SpanVariant variant)
         {
-            public Regex Pattern = pattern;
-            public SpanVariant Variant = variant;
+			/// <summary>
+			/// The <see cref="Regex"/> pattern to match with.
+			/// </summary>
+			public Regex Pattern = pattern;
+			/// <summary>
+			/// The <see cref="SpanVariant"/> that the pattern applies to.
+			/// </summary>
+			public SpanVariant Variant = variant;
         }
-
-        public class MatchResult(
+		/// <summary>
+		/// Represents a match result from a <see cref="QuoteRegex"/>.
+		/// </summary>
+		/// <param name="index"><inheritdoc cref="Index" path="/summary"/></param>
+		/// <param name="value"><inheritdoc cref="Value" path="/summary"/></param>
+		/// <param name="inner"><inheritdoc cref="Inner" path="/summary"/></param>
+		/// <param name="variant"><inheritdoc cref="Variant" path="/summary"/></param>
+		/// <param name="isConstrained"><inheritdoc cref="IsConstrained" path="/summary"/></param>
+		protected class MatchResult(
             int index,
             string value,
             string inner,
@@ -19,49 +37,165 @@ namespace AsciiDocSharp
             bool isConstrained = true
         )
         {
-            public int Index = index;
-            public string Value = value;
-            public string Inner = inner;
-            public SpanVariant? Variant = variant;
-            public bool IsConstrained = isConstrained;
+			/// <summary>
+			/// The position of the first character of the match.
+			/// </summary>
+			public int Index = index;
+			/// <summary>
+			/// The matching value, including the formatting marks.
+			/// </summary>
+			public string Value = value;
+			/// <summary>
+			/// The matching value, excluding the formatting marks.
+			/// </summary>
+			public string Inner = inner;
+			/// <summary>
+			/// The <see cref="SpanVariant"/> that the result represents.
+			/// </summary>
+			public SpanVariant? Variant = variant;
+			/// <summary>
+			/// Whether or not the result is constrained.
+			/// </summary>
+			public bool IsConstrained = isConstrained;
         }
 
-        public static readonly Regex PassConstrained = RxPassCon();
+        /// <summary>
+        /// Matches any constrained <see langword="string"/> surrounded by <c>+</c>s.
+        /// </summary>
+        protected static readonly Regex PassConstrained = RxPassthroughConstrained();
 
-        public static readonly Regex PassUnconstrained = RxPass();
+		/// <summary>
+		/// Matches any <see langword="string"/> surrounded by <c>++</c>s.
+		/// </summary>
+		protected static readonly Regex PassUnconstrained = RxPassthrough();
 
-        public static readonly QuoteRegex[] ConstrainedQuoteRegexes =
+		/// <summary>
+		/// Represents a collection of <see cref="QuoteRegex"/> patterns that are constrained.
+		/// </summary>
+		/// <remarks>
+		/// Matches the following formatting marks and their corresponding <see cref="SpanVariant"/>s in the following order:
+		/// <list type="bullet">
+		/// <item>
+		/// <term>`+ +`</term>
+		/// <description><see cref="SpanVariant.LiteralMonospace"/></description>
+		/// </item>
+		/// <item>
+		/// <term># #</term>
+		/// <description><see cref="SpanVariant.Mark"/></description>
+		/// </item>
+		/// <item>
+		/// <term>` `</term>
+		/// <description><see cref="SpanVariant.Monospace"/></description>
+		/// </item>
+		/// <item>
+		/// <term>* *</term>
+		/// <description><see cref="SpanVariant.Strong"/></description>
+		/// </item>
+		/// <item>
+		/// <term>_ _</term>
+		/// <description><see cref="SpanVariant.Emphasis"/></description>
+        /// </item>
+        /// </list>
+        /// </remarks>
+		protected static readonly QuoteRegex[] ConstrainedQuoteRegexes =
         [
-            new(RxLitMonoCon(), SpanVariant.LiteralMonospace),
-            new(RxSubCon(), SpanVariant.Subscript),
-            new(RxSupCon(), SpanVariant.Superscript),
-            new(RxMarkCon(), SpanVariant.Mark),
-            new(RxMonoCon(), SpanVariant.Monospace),
-            new(RxStrongCon(), SpanVariant.Strong),
-            new(RxEmCon(), SpanVariant.Emphasis),
+            new(RxLiteralMonoSpaceConstrained(), SpanVariant.LiteralMonospace),
+            new(RxMarkConstrained(), SpanVariant.Mark),
+            new(RxMonospaceConstrained(), SpanVariant.Monospace),
+            new(RxStrongConstrained(), SpanVariant.Strong),
+            new(RxEmphasisConstrained(), SpanVariant.Emphasis),
         ];
 
-        public static readonly QuoteRegex[] UnconstrainedQuoteRegexes =
-        [
-            new(RxMark(), SpanVariant.Mark),
-            new(RxMono(), SpanVariant.Monospace),
+		/// <summary>
+		/// Represents a collection of <see cref="QuoteRegex"/> patterns that are unconstrained.
+		/// </summary>
+		/// <remarks>
+		/// Matches the following formatting marks and their corresponding <see cref="SpanVariant"/>s in the following order:
+		/// <list type="bullet">
+		/// <item>
+		/// <term>~ ~</term>
+		/// <description><see cref="SpanVariant.Subscript"/></description>
+		/// </item>
+		/// <item>
+		/// <term>^ ^</term>
+		/// <description><see cref="SpanVariant.Superscript"/></description>
+		/// </item>
+		/// <item>
+		/// <term>## ##</term>
+		/// <description><see cref="SpanVariant.Mark"/></description>
+		/// </item>
+		/// <item>
+		/// <term>`` ``</term>
+		/// <description><see cref="SpanVariant.Monospace"/></description>
+		/// </item>
+		/// <item>
+		/// <term>** **</term>
+		/// <description><see cref="SpanVariant.Strong"/></description>
+		/// </item>
+		/// <item>
+		/// <term>__ __</term>
+		/// <description><see cref="SpanVariant.Emphasis"/></description>
+		/// </item>
+		/// </list>
+		/// </remarks>
+		protected static readonly QuoteRegex[] UnconstrainedQuoteRegexes =
+		[
+			new(RxSubscript(), SpanVariant.Subscript),
+			new(RxSuperscript(), SpanVariant.Superscript),
+			new(RxMark(), SpanVariant.Mark),
+            new(RxMonospace(), SpanVariant.Monospace),
             new(RxStrong(), SpanVariant.Strong),
-            new(RxEm(), SpanVariant.Emphasis),
+            new(RxEmphasis(), SpanVariant.Emphasis),
         ];
 
-        public class InlineParser(string input)
+		/// <summary>
+		/// Represents a parser for inline elements.
+		/// </summary>
+		/// <remarks>
+		/// This class is to be instantiated with a string to parse, and then the <c>Parse</c> method is called to return the parsed inlines.
+		/// Inline parsing involves apply text formatting to each inline element. Formatting marks come in two forms, <c>constrained</c> and <c>unconstrained</c>.
+		/// Constrained formatting marks are typically single formatting pairs <c>*like* _this_</c>, and surround words or phrases. Constrained formatting marks
+		/// must be preceded by whitespace, and followed by whitespace or punctuation. Unconstrained formatting marks do not have these restrictions, and are typically
+		/// a more verbose version of their constrained counterpart ,<c>**like**__this__</c>. Unconstrained formatting takes precedence over constrained formatting, but
+		/// both are superseded by passthrough formatting, typically used to escape formatting marks.
+		/// <para>
+		/// For more information on text formatting, see the <see href="https://docs.asciidoctor.org/asciidoc/latest/text/">documentation on text formatting</see>.
+		/// </para>
+		/// </remarks>
+		/// <param name="input">The text to parse.</param>
+		public class InlineParser(string input)
         {
-            public string Input = input;
-            private string MutInput = input;
-            public List<MatchResult> Matches = [];
-            public List<BaseInline> inlines = [];
+			/// <summary>
+			/// An immutable representation of the input text.
+			/// </summary>
+			private readonly string Input = input;
+			/// <summary>
+			/// A mutable representation of the input text. When matches are found, they are replaced with null characters to avoid re-matching.
+			/// </summary>
+			private string MutInput = input;
+			/// <summary>
+			/// A collection of matches found during parsing.
+			/// </summary>
+			private readonly List<MatchResult> Matches = [];
+			/// <summary>
+			/// A collection of the parsed inlines to be returned after parsing.
+			/// </summary>
+			private readonly List<BaseInline> inlines = [];
 
-            public BaseInline[] Parse()
+			/// <summary>
+			/// Parses the input text and returns the formatted inlines.
+			/// </summary>
+			/// <returns>An array of the formatted inlines.</returns>
+			public BaseInline[] Parse()
             {
                 return this.Pass().UnconstrainedQuotes().ConstrainedQuotes().Finish();
             }
 
-            public InlineParser Pass()
+			/// <summary>
+			/// Applies passthrough formatting to the input text.
+			/// </summary>
+			/// <returns><see langword="this"/></returns>
+			private InlineParser Pass()
             {
                 foreach (Match m in PassUnconstrained.Matches(MutInput))
                 {
@@ -76,7 +210,11 @@ namespace AsciiDocSharp
                 return this;
             }
 
-            public InlineParser UnconstrainedQuotes()
+			/// <summary>
+			/// Applies unconstrained formatting to the input text.
+			/// </summary>
+			/// <returns><see langword="this"/></returns>
+			private InlineParser UnconstrainedQuotes()
             {
                 foreach (var pair in UnconstrainedQuoteRegexes)
                 {
@@ -89,7 +227,11 @@ namespace AsciiDocSharp
                 return this;
             }
 
-            public InlineParser ConstrainedQuotes()
+			/// <summary>
+			/// Applies constrained formatting to the input text.
+			/// </summary>
+			/// <returns><see langword="this"/></returns>
+			public InlineParser ConstrainedQuotes()
             {
                 foreach (var pair in ConstrainedQuoteRegexes)
                 {
@@ -102,9 +244,17 @@ namespace AsciiDocSharp
                 return this;
             }
 
-            public BaseInline[] Finish()
+			// TODO: review why SpanVariant is nullable and why we have varying fallbacks
+			// TODO: ensure there's logic to handle what kind of inlines can be nested in other inlines
+			/// <summary>
+			/// Constructs and returns a collection of the formatted inlines.
+			/// </summary>
+			/// <returns>An array of the formatted inlines.</returns>
+			private BaseInline[] Finish()
             {
-                string[] outArr =
+				// Split the input text by the matches to preserve the original text
+				// and sort the matches by index to maintain original order
+				string[] outArr =
                     Matches.Count > 0
                         ? Input.Split(
                             Matches.Select(match => match.Value).ToArray(),
@@ -117,21 +267,26 @@ namespace AsciiDocSharp
                 {
                     if (!String.IsNullOrEmpty(outArr[i]))
                     {
-                        InlineLiteral literal = new(ElementType.Paragraph, outArr[i]);
+						// If string isn't empty, then we didn't find a match in formatting.
+						// We can assume it's a literal string and add it to the inlines.
+						InlineLiteral literal = new(ElementType.Paragraph, outArr[i]);
                         inlines.Add(literal);
                     }
                     if (i < Matches.Count)
                     {
                         if (Matches[i].Variant is null)
                         {
-                            InlineLiteral literal = new(ElementType.Pass, Matches[i].Value);
+							// If it was matched but had a null variant, we can assume it's a passthrough
+							InlineLiteral literal = new(ElementType.Pass, Matches[i].Value);
                         }
                         else
                         {
-                            InlineParser innerParser = new(Matches[i].Inner);
-                            InlineSpan span =
+							// Do another pass on the inner text to parse any nested inlines
+							InlineParser innerParser = new(Matches[i].Inner);
+							InlineSpan span =
                                 new(
-                                    Matches[i].Variant ?? SpanVariant.Mark,
+									// Fallback to a mark if the variant is null
+									Matches[i].Variant ?? SpanVariant.Mark,
                                     Matches[i].IsConstrained,
                                     innerParser.Parse()
                                 );
@@ -142,7 +297,14 @@ namespace AsciiDocSharp
                 return [.. inlines];
             }
 
-            private void AddMatch(Match match, SpanVariant? variant, bool isConstrained)
+			/// <summary>
+			/// Parses a <see cref="Match"/> into a <see cref="MatchResult"/>, adds it to the <see cref="Matches"/> collection,
+			/// and replaces the match in the <see cref="MutInput"/>.
+			/// </summary>
+			/// <param name="match">The match to be added.</param>
+			/// <param name="variant">The type of <see cref="SpanVariant"/> of the match.</param>
+			/// <param name="isConstrained">Whether or not the match is constrained.</param>
+			private void AddMatch(Match match, SpanVariant? variant, bool isConstrained)
             {
                 Matches.Add(
                     new MatchResult(
@@ -162,43 +324,44 @@ namespace AsciiDocSharp
             }
         }
 
+
         [GeneratedRegex(@"\+\+\s*?([^(\+\+)]+)\+\+")]
-        private static partial Regex RxPass();
+        private static partial Regex RxPassthrough();
 
         [GeneratedRegex(@"(?<=\s|^)\+\s*?([^+\00]+)\+(?=\s|[\p{P}])")]
-        private static partial Regex RxPassCon();
+        private static partial Regex RxPassthroughConstrained();
 
         [GeneratedRegex(@"(?<=\s|^)`\+\s*?([^`\+\00]+)\+`(?=\s|[\p{P}])")]
-        private static partial Regex RxLitMonoCon();
+        private static partial Regex RxLiteralMonoSpaceConstrained();
 
         [GeneratedRegex(@"~\s*?([^~\00\s]+)~")]
-        private static partial Regex RxSubCon();
+        private static partial Regex RxSubscript();
 
         [GeneratedRegex(@"\^\s*?([^\^\00\s]+)\^")]
-        private static partial Regex RxSupCon();
+        private static partial Regex RxSuperscript();
 
         [GeneratedRegex(@"(?<=\s|^)#\s*?([^#\00]+)#(?=\s|[\p{P}]|$)")]
-        private static partial Regex RxMarkCon();
+        private static partial Regex RxMarkConstrained();
 
         [GeneratedRegex(@"(?<=\s|^)`\s*?([^`\00]+)`(?=\s|[\p{P}]|$)")]
-        private static partial Regex RxMonoCon();
+        private static partial Regex RxMonospaceConstrained();
 
         [GeneratedRegex(@"(?<=\s|^)\*\s*?([^\*\00]+)\*(?=\s|[\p{P}]|$)")]
-        private static partial Regex RxStrongCon();
+        private static partial Regex RxStrongConstrained();
 
         [GeneratedRegex(@"(?<=\s|^)_\s*?([^_\00]+)_(?=\s|[\p{P}]|$)")]
-        private static partial Regex RxEmCon();
+        private static partial Regex RxEmphasisConstrained();
 
         [GeneratedRegex(@"##\s*?([^\00(##)]+)##")]
         private static partial Regex RxMark();
 
         [GeneratedRegex(@"``\s*?([^\00(``)]+)``")]
-        private static partial Regex RxMono();
+        private static partial Regex RxMonospace();
 
         [GeneratedRegex(@"\*\*\s*?([^\00(\*\*)]+)\*\*")]
         private static partial Regex RxStrong();
 
         [GeneratedRegex(@"__\s*?([^\00(__)]+)__")]
-        private static partial Regex RxEm();
+        private static partial Regex RxEmphasis();
     }
 }
